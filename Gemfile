354676134f3ebd146c7d4f1d13701cb79730f3ac
taro.inu <inu1973dog.mrn@gmail.com>
2020-05-10 12:10:17 +0000

    user signup

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ group :production do
   gem 'pg', '>= 0.18', '< 2.0'
 end
 
+gem 'bcrypt', '~> 3.1.7'
+gem 'kaminari'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
   
+  get 'toppages/index'
+  root to: 'tasks#index'
+
+  root to: 'tappages#index'
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
