1c9a8d7dc2ae85799aace767c1d4069320e2d8d1
taro.inu <inu1973dog.mrn@gmail.com>
2020-05-11 07:44:38 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'toppages/index'
   root to: 'tasks#index'
 
   root to: 'tappages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   
   get 'signup', to: 'users#new'
   resources :tasks
